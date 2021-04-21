85f6f8ac6a68887025617f12cf365398031ceb3d
Guy Shimony <shimonyguy@gmail.com>
2021-04-21 16:37:45 +0300

    update character limit

--- app.js
+++ app.js
@@ -305,7 +305,7 @@ function limit(element)
     var max_chars = 1;
 
     if(element.value.length > max_chars) {
-        element.value = element.value.substr(0, max_chars);
+        element.value = element.value.substr(1, max_chars);
     }
 }
 
